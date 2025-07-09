c13277456bff69ee672c942a5df3dec38c9d36e8
Denis585585 <Vegan585@yandex.ru>
2025-07-10 00:54:00 +0300

    Правка в аннотации Boot классов

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/ShoppingStoreApplication.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/ShoppingStoreApplication.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.shoppingstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import ru.yandex.practicum.iteractionapi.client.ShoppingStoreClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(clients = ShoppingStoreClient.class)
 public class ShoppingStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStoreApplication.class, args);
