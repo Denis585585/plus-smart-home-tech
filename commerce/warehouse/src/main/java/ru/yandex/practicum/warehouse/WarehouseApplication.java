1cc4590e362eee60f5c27a1391e703d50b53c186
Denis585585 <Vegan585@yandex.ru>
2025-07-09 23:35:00 +0300

    Правка в аннотации Boot классов

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApplication.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import ru.yandex.practicum.iteractionapi.client.ShoppingStoreClient;
+import ru.yandex.practicum.iteractionapi.client.WarehouseClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {ShoppingStoreClient.class})
+@EnableFeignClients(clients = {WarehouseClient.class})
 public class WarehouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
