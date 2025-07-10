43e13e6b1b7d788dadfbcb5f279c832ff339630c
Denis585585 <Vegan585@yandex.ru>
2025-07-10 22:44:57 +0300

    Переименование Interaction-api

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/shoppingcart/ShoppingCartApplication.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/shoppingcart/ShoppingCartApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.yandex.practicum.interactionapi.client.WarehouseClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {WarehouseClient.class})
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interactionapi.client")
 public class ShoppingCartApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
