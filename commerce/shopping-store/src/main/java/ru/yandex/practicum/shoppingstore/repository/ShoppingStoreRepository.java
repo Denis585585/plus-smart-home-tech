afe0847f7484ac91a1a7d65a871d18ac7270ef4d
Denis585585 <Vegan585@yandex.ru>
2025-07-12 19:00:46 +0300

    Убрал фолбэк

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/repository/ShoppingStoreRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/repository/ShoppingStoreRepository.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.shoppingstore.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.interactionapi.enums.ProductCategory;
 import ru.yandex.practicum.shoppingstore.model.Product;
@@ -13,8 +12,6 @@ import java.util.UUID;
 
 @Repository
 public interface ShoppingStoreRepository extends JpaRepository<Product, UUID> {
-
-    @Query("SELECT p FROM Product p WHERE p.productCategory = :category")
     List<Product> findAllByProductCategory(ProductCategory productCategory, Pageable pageable);
 
     Optional<Product> findByProductId(UUID productId);
