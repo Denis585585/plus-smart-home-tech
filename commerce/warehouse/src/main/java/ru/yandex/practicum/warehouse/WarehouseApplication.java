16ede094fa7de824b97777d6a532407a8bac6ace
Denis585585 <Vegan585@yandex.ru>
2025-07-13 22:32:09 +0300

    Fix

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApplication.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.yandex.practicum.interactionapi.client.ShoppingStoreClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {ShoppingStoreClient.class})
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interactionapi.client")
 public class WarehouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
