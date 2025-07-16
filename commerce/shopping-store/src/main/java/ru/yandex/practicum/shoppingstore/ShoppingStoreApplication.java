5a274d4f561d3cb7485e516e0afe0e865a80628b
Denis585585 <Vegan585@yandex.ru>
2025-07-16 21:00:09 +0300

    Правки по рекомендациям

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/ShoppingStoreApplication.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/ShoppingStoreApplication.java
@@ -2,15 +2,13 @@ package ru.yandex.practicum.shoppingstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@ConfigurationPropertiesScan
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interactionapi.client")
 public class ShoppingStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStoreApplication.class, args);
